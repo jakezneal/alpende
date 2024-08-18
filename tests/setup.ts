01427817d8c337d1db8ad924a5a9fa85c1129815
Jake Neal <hello@jakeneal.co.uk>
2024-08-18 19:19:22 +0100

    :heavy_plus_sign: Added @testing-library/vue and testing-library-selector
    Swapped out our custom test helpers in favour of `testing-library-selector`. Also set up project annotations so we can utilise portable stories with Testing Library and Vitest

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,34 +1,21 @@
 import { config, createWrapperError, DOMWrapper, type VueWrapper } from '@vue/test-utils';
+import { beforeAll } from 'vitest';
+import { render as testingLibraryRender } from '@testing-library/vue';
+import { setProjectAnnotations } from '@storybook/vue3';
+import * as previewAnnotations from '../.storybook/preview';
+
 import test from '~~/src/directives/test';
+
+const annotations = setProjectAnnotations([
+    previewAnnotations,
+    {
+        testingLibraryRender,
+    },
+]);
+
+beforeAll(annotations.beforeAll!);
 
 // Add test directives to global config.
 config.global.directives = {
     test,
 };
-
-export interface TestWrapper extends VueWrapper {
-    findByTestId: (id: string) => DOMWrapper<Element>;
-    findAllByTestId: (id: string) => DOMWrapper<Element>[];
-    findComponentByTestId: (component: string, id: string) => object;
-}
-
-// Set up test helpers.
-const addTestIdHelpers = (wrapper: VueWrapper<any>) => {
-    const selector = (id: string) => `[data-testid="${id}"]`;
-
-    const findByTestId = (id: string) => wrapper.find(selector(id));
-
-    const findAllByTestId = (id: string) => wrapper.findAll(selector(id));
-
-    const findComponentByTestId = (component: string, id: string) =>
-        wrapper.findAllComponents(component)?.find((w) => w.attributes('data-testid') === id) ||
-        createWrapperError('DOMWrapper');
-
-    return {
-        findByTestId,
-        findAllByTestId,
-        findComponentByTestId,
-    };
-};
-
-config.plugins.VueWrapper.install(addTestIdHelpers as any);
